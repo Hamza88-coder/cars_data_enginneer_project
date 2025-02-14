5a81dc67b8c4e4009116c67c38693799984f1228
Hamza88-coder <hamzamalyana089@gmil.com>
2025-02-14 09:59:55 +0100

    update

---
+++ creation_tab.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Vehicles (
+    Make VARCHAR(50),
+    Model VARCHAR(50),
+    Type_ VARCHAR(50),
+    Origin VARCHAR(50),
+    DriveTrain VARCHAR(50),
+    MSRP DECIMAL(10, 2),
+    Invoice DECIMAL(10, 2),
+    EngineSize DECIMAL(5, 2),
+    Cylinders INT,
+    Horsepower INT,
+    MPG_City INT,
+    MPG_Highway INT,
+    Weight_ INT,
+    Wheelbase INT,
+    Length INT
+);
